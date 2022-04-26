fbb06bfbd8749c61d1bd7970fbe4a72103eb8189
Pedro Chiachio <phcc1399@outlook.com>
2022-04-26 16:19:10 -0300

    Data Update and Revision - Day 3 - Docstrings

--- systools/data.py
+++ systools/data.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Apr 12 10:28:14 2022
 
-@author: bcalazans
+@author: bcalazans & MoleDownTheHole (pcardoso)
 
 Módulo com funções úteis no tratamento de dados tabulados.
 
@@ -18,7 +18,33 @@ import systools.helpers.readWrite as rW
 
 def dataframe2numeric(df, col_dt_preffix=None, col_td_preffix=None):
 
-    """Transform the dataFrame to numeric and time formats..."""
+    """
+    Format the DataFrame columns to Numeric and Time Formats.
+    Support datetime, timestamp, int and float formats.
+
+    Parameters
+    ----------
+    df : DataFrame
+        DataFrame type variable that has columns who will be converted
+        to numeric or time format.
+
+    col_dt_preffix : string, optional
+        String type variable that have the prefixes used to show that
+        the designated column title that contains it,
+        must be converted to datetime type.
+
+    col_td_preffix : string, optional
+        String type variable that have the prefixes used to show that
+        the designated column title that contains it,
+        must be converted to timedelta type.
+
+    Returns
+    -------
+
+    df : DataFrame
+    DataFrame used in the input, but with the numeric and time columns converted.
+
+    """
 
     other_cols = [col for col in df.columns if df[col].dtype != 'datetime64[ns]']
     all_cols = df.columns
@@ -56,34 +82,36 @@ def dataframe2numeric(df, col_dt_preffix=None, col_td_preffix=None):
     return df
 
 
-def open_file(path, name=None, expected_cols=None, usa=False, col_td_preffix='gap',  col_dt_preffix='H_', **kwargs):
+def open_file(path, name=None, expected_cols=None, usa=False, col_td_prefix='gap',  col_dt_prefix='H_', **kwargs):
 
     """
-    Open file as a geopandas.DataFrame.
+    Open file as a DataFrame.
     Support csv, txt, parquet, excel, shape, dbf and ziped files.
 
     Parameters
     ----------
     path : string
-        Path of the file, may include the file name with extension
-        If the file is zipped, pass the path with the zip extension
+        Path of the file, may include the file name with extension.
+        If the file is zipped, pass the path with the zip extension.
+
     name : string, optional
-        Name of the file with extension to be open. 
-        The default is None. Expects that the name was passed on the path arg
+        Name of the file with extension to be open.
+        Expects that the name was passed on the path arg.
+
     expected_cols : list, optional
-        Import only this cols. The default is None.
+        Import only this cols.
     usa : Bool, optional
-        If TRUE expects comma separetor and point as decimal indicator. 
-        If FALSE expects ; as separetor and comma as decimal indicator.
-        The default is False.
-    col_dt_preffix: String, optional
-        Indicates column name preffix to be converted to datetime.
-        The default is 'H_'
-    col_td_preffix: String, optional
-        BRUNA ESCREVER SIGNIFICADO AQUI
-        The default is 'gap'.
+        If TRUE expects comma separator and point as decimal indicator.
+        If FALSE expects ; as separator and comma as decimal indicator.
+
+    col_dt_prefix: String, optional
+        Indicates column name prefix to be converted to datetime.
+
+    col_td_prefix: String, optional
+        Indicates column name prefix to be converted to timedelta.
+
     **kwargs : dict
-        Any argment for the original read functions from (geo)pandas.
+        Any argument for the original read functions from (geo)pandas.
 
     Returns
     -------
@@ -95,7 +123,7 @@ def open_file(path, name=None, expected_cols=None, usa=False, col_td_preffix='ga
     df = rW.load_file(path, name, expected_cols, usa, **kwargs)
          
     # configure formats
-    df = dataframe2numeric(df, col_dt_preffix, col_td_preffix)
+    df = dataframe2numeric(df, col_dt_prefix, col_td_prefix)
     print(f'{currentframe().f_code.co_name} {textwrap.shorten(name, width=50):50} {len(df):>9,}')
     return df
 
@@ -104,14 +132,45 @@ def open_file(path, name=None, expected_cols=None, usa=False, col_td_preffix='ga
 
 def save_file(df, path, name='test', usa=False, ext='csv',
               sheet_name='Sheet1', start_row=None, truncate_sheet=False):
+    """
+    Save DataFrame to an exterior file created and formated based
+    on various parameters.
+    Support csv, txt, parquet, excel and shape.
 
-    # Salva o arquivo em CSV (default) ou SHP (defalut se tiver geometria).
-    # Pode escolher tbm as outras coisas
+    Parameters
+    ----------
+    df : DataFrame
+        DataFrame type variable that has columns who the user
+        wants to export.
 
-    # If ext=='excel' :
-    # Truncate_sheet.
-    # False: append data to existing sheet on the start_row.
-    # True: delete sheet and paste data on a new one.
+    path : String
+        Path of the file.
+        Doesn't need to have the filename and extension included.
+
+    name : String, optional
+        File name.
+        Doesn't need to have the extension included.
+
+    usa : Boolean, optional
+        True -> The exported file uses the american format.
+        (Ex: Comma as separator, dot as decimal...)
+        False -> The exported file doesn't uses the american format.
+        (Ex: Semicolon as separator, comma as decimal...)
+
+    ext : String, optional
+        The file extension.
+        Must be csv, txt, parquet, excel or shape.
+
+    sheet_name : String, optional
+        The file, when saved as an excel file, sheet name.
+
+    start_row : Integer, optional
+        The excel file row where the user wants the new data to be inserted.
+
+    truncate_sheet : Boolean, optional
+        Remove and recreate the destination sheet of the exported excel file.
+
+    """
             
     extensions = ['shp', 'csv', 'parquet', 'excel']
 
@@ -130,14 +189,34 @@ def save_file(df, path, name='test', usa=False, ext='csv',
 
 
 def get_col(df, get_cols, from_df, key, key2=None):
+
     """
-    Add cols on df
-    param df: data frame to add cols on.
-    param get_cols: columns from other data frame you want the info.
-    param from_df: data frame that you will look up the info (get_cols).
-    param key: key to merge (on parameter from merge method).
-    key2: BRUNA ESCREVER SIGNIFICADO AQUI.
-    return: df with columns get_cols added.
+    Add columns to an input DataFrame, originated from another DataFrame
+    based on a merge correspondence with in common column.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Main input dataframe that will receive the merged columns.
+
+    get_cols : List
+        Columns from the other input dataframe you want to merge.
+
+    from_df : DataFrame
+        Secondary input dataframe that will be used to get the
+        columns to be merged.
+
+    key : String
+        Column in main DataFrame used as key to merge the two DataFrames.
+
+    key2 : String, optional
+        Column in secondary DataFrame used as key to merge the two DataFrames.
+
+    Returns
+    -------
+    ans : DataFrame
+        Main input DataFrame with the merged columns.
+
     """
 
     if isinstance(key, str):
@@ -180,6 +259,31 @@ def get_col(df, get_cols, from_df, key, key2=None):
 
 
 def get_mask_isin(df, key, base_data):
+
+    """
+    Get a filter mask to be used in a DataFrame based on
+    single ou multiple filter.
+    (Easier than the usual way)
+
+    Parameters
+    ----------
+    df : DataFrame
+        DataFrame that the user want to create a mask.
+
+    key : String or List
+        The column, or columns, that the filter will be applied.
+
+    base_data : List and Tuple
+        List filled with tuples that each one represents one filter.
+
+    Returns
+    -------
+    mask : Series
+        A Series of Boolean values that can be used as a mask for
+        the input DataFrame.
+
+    """
+
     if isinstance(key, list):
         new_col = '_'.join(key)
         my_list_series = []
@@ -194,19 +298,55 @@ def get_mask_isin(df, key, base_data):
 
 
 def remove_duplicate_safe(df, cols):
+
+    """
+    Remove DataFrame duplicates based on a column,
+    or multiple columns, value.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Dataframe that the user want to drop duplicates.
+
+    cols : String or List
+        Column or list of columns used as parameter for
+        the drop_duplicates function.
+
+    Returns
+    -------
+    df : DataFrame
+        DataFrame without the duplicates in cols parameter.
+
+    """
+
     num = len(df)
     df = df.drop_duplicates(cols)
     diff = num-len(df)
     if diff > 0:
-        print('fDuplicates removed from{name:}[{cols:}]{diff:,}{diff/num:,.2f}')
+        print(f'Duplicates removed from [{cols:}]{diff:,}{diff/num:,.2f}')
     return df
 
 
 def flatten_hierarchical_col(col, sep='_'):
+
     """
-    Use after make a groupby operation with agg that cuases a multi level
-    --- USAGE: 
-        df.columns = df.columns.map(flattenHierarchicalCol)
+    Turn multi-index column names into single index
+    separated by '_'.
+
+    Parameters
+    ----------
+
+    col : Multi-Index Column Title
+        Multi-Index column that will be replaced for a single_index column.
+    sep : String, optional
+        Separator for the single_index column name.
+
+    Returns
+    -------
+    new_col : Single_Index Column Title
+        Multi_Index column title passed in the parameters, but flattened
+        as no hierarchical title.
+
     """
 
     if not type(col) is tuple:
