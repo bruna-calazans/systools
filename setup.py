3445612e32a5a93b47af1511141046b4ec45647f
Pedro Chiachio <107192516+pcardoso-sk@users.noreply.github.com>
2022-11-01 16:15:41 -0300

    Setup.py file created...

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(
+  
+    name='systool',
+    version='0.04.00',
+    license='MIT',
+    author="Bruna Calazans (bruna-calazans) & Pedro Chiachio (pcardoso-sk)",
+    author_email='bcalazans@systra.com',
+    package_dir={'': 'main'},
+    include_package_data=True,
+    package_data={'utils': ['charts.py', 'flatt_geom.py', 'get_ftp_data.py', 'get_socioEconomic_data.py', 
+                            'maps.py', 'readWrite.py', 'report.py', 'linear_regression_make.py',
+                            'linear_regression_plot.py', 'miscFunctions']},
+    url='https://github.com/bruna-calazans/systools',
+    keywords=['systool', 'engenharia', 'transportes', 'systra', 'engenharia de transportes'],
+    install_requires=['pytest',
+                      'pandas',
+                      'numpy',
+                      'matplotlib',
+                      'matplotlib_scalebar',
+                      'geopandas',
+                      'openpyxl',
+                      'seaborn',
+                      'statsmodels',
+                      'tqdm',
+                      'scipy',
+                      'plotly',
+                      'shapely'],
+)
